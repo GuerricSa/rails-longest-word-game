b6e1858780de42ef7ef609d0a79eeff7bf5abc61
guerric sant <guerric.sant@gmail.com>
2023-10-09 11:42:57 +0200

    modification in controller to correct bug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,10 +15,10 @@ class GamesController < ApplicationController
 
   def score
     @result = 0
-    @grid = params[:letters]
+    @grid = params[:letters].upcase
     @attempt = params[:input].downcase
     @truc = test_attempt_grid(@attempt, @grid)
-    if check_dictionary(@attempt) && test_attempt_grid(@attempt, @grid)
+    if test_attempt_grid(@attempt, @grid) && check_dictionary(@attempt)
       @result = 3
       session[:score].nil? ? session[:score] = @attempt.size : session[:score] += @attempt.size
     elsif test_attempt_grid(@attempt, @grid)
@@ -34,13 +34,11 @@ class GamesController < ApplicationController
   def test_attempt_grid(attempt, grid)
     attempt_hash = attempt.upcase.chars.to_h { |letter| [letter, attempt.upcase.chars.count(letter)] }
     grid_hash = ('A'..'Z').to_h { |letter| [letter, grid.chars.count(letter)] }
-    attempt_hash.all? do |letter, count|
-      grid_hash[letter] <= count
-    end
+    attempt_hash.all? { |letter, count| count <= grid_hash[letter] }
   end
 
   def check_dictionary(attempt)
-    url = "https://wagon-dictionary.herokuapp.com/#{@attempt.strip}"
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt.strip}"
     dictionary = JSON.parse(URI.open(url).read)
     dictionary['found']
   end
